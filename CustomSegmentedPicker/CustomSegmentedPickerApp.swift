1b375fcfbc3c059949cdb7296c965af07e7b0a02
Sasha Myshkina <myshkinasasha@gmail.com>
2025-02-09 20:08:36 +0000

    Initial Commit

---
+++ CustomSegmentedPicker/CustomSegmentedPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSegmentedPickerApp.swift
+//  CustomSegmentedPicker
+//
+//  Created by Sasha Myshkina on 09/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct CustomSegmentedPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
